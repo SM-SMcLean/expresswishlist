3fe9fe89ddf89df971b1641da4fc76871506a5ac
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2024-12-29 19:16:16 +0000

    Update app.js and index.ejs
    updated app.js and index.ejs to provide ability to use static files

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-   const morgan = require('morgan');
-   const PORT = 3000;
-   const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const PORT = 3000;
 
-   app.use(express.static(path.join(__dirname, 'public')));
-   app.use(morgan('tiny'));
-   app.set('view engine', 'ejs');
-   app.get('/', (req, res) => {
-       res.render('index');
-   });
-   app.listen(PORT, (err) => {
-       if (err) return console.log(err);
-console.log(`Express listening on port ${PORT}`); });
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('tiny'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.listen(PORT, (err) => {
+    if (err) return console.log(err);
+    console.log(`Express listening on port ${PORT}`);
+});
