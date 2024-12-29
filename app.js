f94044349d276a6cd3698b8cfbb57e02ae42c9d0
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2024-12-29 20:40:02 +0000

    Update app.js and index.ejs for form handling
    updated app.js to handle form data
    updated index.ejs to include form

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const PORT = 3000;
 
 const wishlist = [
     { "item": "Playstation 5" },
-    { "item": "Some Science Fiction Novels"},
-    { "item": "Cadbury's Selection Box"},
-    { "item": "Super Slinky Guitar Strings"}
+    { "item": "Some Science Fiction Novels" },
+    { "item": "Cadbury's Selection Box" },
+    { "item": "Super Slinky Guitar Strings" }
 ];
 
 const app = express();
@@ -15,11 +15,19 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('tiny'));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', { data: wishlist });
 });
+
+app.post('/', (req, res) => {
+    const newitem = req.body;
+    wishlist.push({ "item": newitem.myitem });
+    res.render('index', { data: wishlist });
+});
+
 app.listen(PORT, (err) => {
     if (err) return console.log(err);
     console.log(`Express listening on port ${PORT}`);
