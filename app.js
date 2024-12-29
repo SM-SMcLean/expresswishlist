5353fed4c9cb280bd54c6bb67a1761cbba972d3f
Stephen McLean <150471173+SM-SMcLean@users.noreply.github.com>
2024-12-29 20:24:40 +0000

    Update app.js and index.ejs to display data
    update app.js to contain array of data
    update index.ejs to display data

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const morgan = require('morgan');
 const path = require('path');
 const PORT = 3000;
 
+const wishlist = [
+    { "item": "Playstation 5" },
+    { "item": "Some Science Fiction Novels"},
+    { "item": "Cadbury's Selection Box"},
+    { "item": "Super Slinky Guitar Strings"}
+];
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,7 +18,7 @@ app.use(morgan('tiny'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { data: wishlist });
 });
 app.listen(PORT, (err) => {
     if (err) return console.log(err);
